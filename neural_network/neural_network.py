0927a8687da9227c1eb0aac9f1c533844f87b089
zahar <zahar.shimanchik@yandex.ru>
2017-04-17 18:24:25 +0300

    neural_network refactoring

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -3,12 +3,9 @@ import tensorflow as tf
 
 
 class NeuralNetwork:
-    def __init__(self, shape, summary_file='tf_log'):
+    def __init__(self, shape, log_dir='tf_log'):
         assert len(shape) == 3  # todo it flexible
-        self._log = bool(summary_file)
-        self.last_input = []
-        self.last_output_with_random = []
-        self.i = 0
+        self._log = bool(log_dir)  # todo do not log if log_dir is not set
 
         self.input = tf.placeholder(tf.float32, [None, shape[0]])
         self.teach_values = tf.placeholder(tf.float32, [None, shape[-1]])
@@ -24,7 +21,7 @@ class NeuralNetwork:
         self.session = tf.InteractiveSession()
         tf.global_variables_initializer().run()
 
-        self.summary_writer = tf.summary.FileWriter(summary_file, graph=self.session.graph)
+        self.summary_writer = tf.summary.FileWriter(log_dir, graph=self.session.graph)
 
     def _init_layers(self, shape):
         with tf.name_scope('hidden1'):
@@ -53,41 +50,10 @@ class NeuralNetwork:
         res = self.session.run(self.output, feed_dict={self.input: x})
         return res
 
-    def train(self, teach_inputs, teach_targets):
+    def train(self, teach_inputs, teach_targets, count=500):
         feed_dict = {
             self.input: teach_inputs,
             self.teach_values: teach_targets,
         }
-        for _ in range(500):
+        for _ in range(count):
             self.session.run(self.train_step, feed_dict)
-
-    def teach_considering_random(self, stimulation_value):
-        """
-        teach network with learn_rate = abs(stimulation_value). calculate with random value should be used before
-        using this method.
-        if stimulation_value > 0 then network teaches like usual by example, which is equals previous calculation result
-        if stimulation_value < 0: then example vector is opposite previous calculation result.
-        :param stimulation_value:
-        """
-        pass
-        # self.i += 1
-        # if stimulation_value < 0:
-        #     answer_with_random = [-x for x in self.last_output_with_random]
-        #     stimulation_value = -stimulation_value
-        # else:
-        #     answer_with_random = self.last_output_with_random
-        #
-        # feed_dict = {
-        #     self.input: self.last_input,
-        #     self.teach_values: [answer_with_random],
-        #     self.learning_rate: stimulation_value,
-        # }
-        #
-        # if self._log:
-        #     (_, summary) = self.session.run([self.train_step, self.summaries], feed_dict)
-        #     self.summary_writer.add_summary(summary, self.i)
-        # else:
-        #     self.session.run(self.train_step, feed_dict)
-
-
-
